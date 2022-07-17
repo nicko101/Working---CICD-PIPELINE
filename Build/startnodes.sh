6aae1057250dff56aea7a03c01b5266dabeb150c
Oran101 <62725475+nicko101@users.noreply.github.com>
2022-07-17 12:34:01 +0100

    Update startnodes.sh

--- Build/startnodes.sh
+++ Build/startnodes.sh
@@ -2,4 +2,4 @@ pip3 install pytest
 pip3 install paramiko
 python3 ./Build/eve_setup.py --topo 	Test-API-CICD/CSR.unl -u
 sleep 10
-#ansible-playbook ./jenkins_play.yml --limit testers
+ansible-playbook ./jenkins_play.yml --limit testers
